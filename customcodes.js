89bc6a9a823dfa14b6253fd12feae5ed025b95f5
DanielLemes <le.mes06@yahoo.com.br>
2017-01-18 16:09:05 -0200

    Update customcodes.js

--- customcodes.js
+++ customcodes.js
@@ -1,35 +1,34 @@
 (function() {
 	"use strict";
-    tinymce.PluginManager.add( 'tt_shortcode_plugin', function( editor, url ) {
-        editor.addButton( 'tt_button_key', {
-            type: 'listbox',
-            text: 'Shortcodes',
-            values: [
-				{ // CAIXAS
-					text: 'Caixas',
-					icon: 'cube',
-					onclick: function() {
-						var get_select;
-						if (tinyMCE.activeEditor.selection.getContent() === '' ) {
-							get_select = '';
-						}
-						else {
-							get_select = tinyMCE.activeEditor.selection.getContent();
-						}
-						
-						editor.windowManager.open({
-							title: 'Configuração da Caixa',
-							body: [
-								{
-									type:'textbox',
-									name: 'caixa_conteudo',
-									label: 'Conteúdo',
-									value: get_select
-								}
-							]
-						});
+	tinymce.PluginManager.add( 'tt_shortcode_plugin', function( editor, url ) {
+		editor.addButton( 'tt_button_key', {
+		    type: 'listbox',
+		    text: 'Shortcodes',
+		    values: [
+			    { // CAIXAS
+				text: 'Caixas',
+				icon: 'cube',
+				onclick: function() {
+					var get_select;
+					if (tinyMCE.activeEditor.selection.getContent() === '' ) {
+						get_select = '';
 					}
-				}]
-		});
-	} );
+					else {
+						get_select = tinyMCE.activeEditor.selection.getContent();
+					}
+					editor.windowManager.open({
+						title: 'Configuração da Caixa',
+						body: [
+							{
+								type:'textbox',
+								name: 'caixa_conteudo',
+								label: 'Conteúdo',
+								value: get_select
+							}
+						]
+					});
+				}
+			}]
+	});
+} );
 } )();
